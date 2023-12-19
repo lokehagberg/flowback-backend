39037d0774ffdd89955a8d6244abfd9a13299bd3
gofven <cratedcreations@gmail.com>
2023-12-19 15:36:40 +0100

    Fixed poll_proposal_list

--- flowback/poll/selectors/proposal.py
+++ flowback/poll/selectors/proposal.py
@@ -1,5 +1,5 @@
 import django_filters
-from django.db.models import F, Subquery, Count
+from django.db.models import F, Subquery, Count, Func
 from flowback.common.services import get_object
 from flowback.poll.models import Poll, PollProposal, PollVotingTypeCardinal
 from flowback.user.models import User
@@ -54,13 +54,17 @@ def poll_proposal_list(*, fetched_by: User, poll_id: int, filters=None):
         if not poll.public:
             group_user_permissions(group=poll.created_by.group.id, user=fetched_by)
 
-        positive_subquery = PollVotingTypeCardinal.objects.filter(author__poll=poll, score__gt=0)
-        negative_subquery = PollVotingTypeCardinal.objects.filter(author__poll=poll, score__lt=0)
+        positive_subquery = PollVotingTypeCardinal.objects.filter(author__poll=poll, score__gt=0
+                                                                  ).annotate(count=Func(F('id'), function='Count')
+                                                                             ).values('count')
+        negative_subquery = PollVotingTypeCardinal.objects.filter(author__poll=poll, score__lt=0
+                                                                  ).annotate(count=Func(F('id'), function='Count')
+                                                                             ).values('count')
 
         filters = filters or {}
         qs = (PollProposal.objects.filter(created_by__group_id=poll.created_by.group.id, poll=poll)
-            .annotate(approval_postitive=Subquery(Count(positive_subquery)),
-                      approval_negative=Subquery(Count(negative_subquery)))
+            .annotate(approval_postitive=Subquery(positive_subquery),
+                      approval_negative=Subquery(negative_subquery))
             .order_by(F('score').desc(nulls_last=True)).all())
 
         if poll.poll_type == Poll.PollType.SCHEDULE:
