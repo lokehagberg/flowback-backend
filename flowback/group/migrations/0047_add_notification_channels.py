6aea810e0617801a28f459dfb0f3a796c41e7f08
Gofven <31272508+Gofven@users.noreply.github.com>
2025-06-25 21:31:59 +0200

    Updated and tested broken migrations

--- flowback/group/migrations/0047_add_notification_channels.py
+++ flowback/group/migrations/0047_add_notification_channels.py
@@ -3,9 +3,17 @@ from django.db import migrations
 def add_notification_channels(apps, schema_editor):
     NotificationChannel = apps.get_model('notification', 'NotificationChannel')
     Group = apps.get_model('group', 'Group')
+    ContentType = apps.get_model('contenttypes', 'ContentType')
+    group_content_type = ContentType.objects.get_for_model(Group)
 
     for group in Group.objects.all():
-        NotificationChannel.objects.get_or_create(content_type=Group, object_id=group.id)
+        if NotificationChannel.objects.filter(content_type=group_content_type, object_id=group.id).exists():
+            continue
+        channel = NotificationChannel(content_type=group_content_type, object_id=group.id)
+        channel.full_clean()
+        channel.save()
+
+        # NotificationChannel.objects.get_or_create(content_type=group)
 
 class Migration(migrations.Migration):
 
