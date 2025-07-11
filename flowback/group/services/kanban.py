f57c95d64001cea19f8d39dff65ea1f46714bcc0
Gofven <31272508+Gofven@users.noreply.github.com>
2025-07-11 16:14:42 +0200

    Updated group_kanban_entry_delete (#275)

--- flowback/group/services/kanban.py
+++ flowback/group/services/kanban.py
@@ -96,7 +96,7 @@ def group_kanban_entry_delete(*,
                               fetched_by_id: int,
                               group_id: int,
                               entry_id: int):
-    kanban_entry = KanbanEntry.objects.get(id=entry_id, origin_type='group', origin_id=group_id)
+    kanban_entry = KanbanEntry.objects.get(id=entry_id, kanban__origin_type='group', kanban__origin_id=group_id)
     group_user_permissions(user=fetched_by_id,
                            group=group_id,
                            permissions=['admin', 'delete_kanban_task'],
