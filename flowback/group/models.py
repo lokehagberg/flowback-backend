b0f4d6daa428895a2bf019a23b3ff7315fb7ef5f
Gofven <31272508+Gofven@users.noreply.github.com>
2025-11-03 12:59:07 +0100

    Update models.py

--- flowback/group/models.py
+++ flowback/group/models.py
@@ -29,7 +29,7 @@ class GroupFolder(BaseModel):
 
 # Permission class for each Group
 class GroupPermissions(BaseModel):
-    role_name = models.TextField(default='default', blank=False)
+    role_name = models.TextField(default='default')
     author = models.ForeignKey('Group', on_delete=models.CASCADE, null=True, blank=True)
     invite_user = models.BooleanField(default=False)
     create_poll = models.BooleanField(default=True)
