0e0260ab7392da2db9dd8b04b54f258fed52294f
Kattenelvis <53380638+Kattenelvis@users.noreply.github.com>
2025-11-04 17:23:18 +0100

    Fixed error 500. Getting length from participant list is enough

--- flowback/chat/views.py
+++ flowback/chat/views.py
@@ -64,7 +64,6 @@ class MessageChannelPreviewAPI(APIView):
     class OutputSerializer(serializers.Serializer):
         id = serializers.IntegerField()
         timestamp = serializers.DateTimeField(allow_null=True)
-        total_participants = serializers.IntegerField()
         channel_id = serializers.IntegerField()
         channel_title = serializers.CharField(source="channel.title")
         participants = SerializerMethodField(help_text="List of Users who participated in the channel, max 20 displayed")
