a2eec0b9d68e613d5e85f8783bb3dd13c9e3f8be
Gofven <31272508+Gofven@users.noreply.github.com>
2025-11-04 13:08:26 +0100

    WIP Schedule

--- flowback/schedule/services.py
+++ flowback/schedule/services.py
@@ -74,6 +74,52 @@ def delete_event(*,
 
     event.delete()
 
+
 # Subscribe (incl. tags, user tags)
+def schedule_notification_subscribe(*,
+                                    user,
+                                    schedule_id: int = None,
+                                    event_id: int = None,
+                                    user_tags: list[str] = None,
+                                    locked: bool = True,
+                                    tags: list[str] = None):
+    """
+    Subscribes user to either a schedule, a tag or an event.
+    :param user: The user that'll subscribe
+    :param schedule_id: The ID of the schedule to subscribe to,
+     used for validation if at least one other field is filled.
+     If schedule_id is the only field set, subscribes to all tags.
+    :param event_id: The ID of the event to subscribe to. Takes priority over schedule and tag subscriptions.
+     Will cause the event subscription to be locked.
+    :param user_tags: List of user-defined tags to subscribe to.
+    :param locked: Whether the event subscription should be locked. Only used for event_id subscriptions.
+    :param tags: List of tags to subscribe to.
+    :return:
+    """
+    if event_id:
+        event = ScheduleEvent.objects.get(id=event_id)
+
+        if schedule_id and event.schedule_id != schedule_id:
+            raise ValidationError('Event does not belong to the schedule')
+
+        event.event_subscribe(user=user,
+                              user_tags=user_tags,
+                              locked=locked)
+    pass
+
 # Unsubscribe
+def schedule_notification_unsubscribe(*,
+                                      user,
+                                      schedule_id: int = None,
+                                      event_id: int = None,
+                                      tags: list[str] = None):
+    """
+    Unsubscribes the user from a schedule, tag or event.
+    :param user: The user that'll unsubscribe
+    :param schedule_id: The ID of the schedule to unsubscribe from. Used for validation.
+     If schedule_id is the only field set, unsubscribes from all tags.
+    :param event_id: The ID of the event to unsubscribe from.
+    :param tags: The tags to unsubscribe from.
+    """
+    pass
 # Update user event (user_tags, locked, reminders)
