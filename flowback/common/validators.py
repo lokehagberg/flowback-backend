83d0013addf6fca370c8714548ae8d82d7d21e84
Gofven <31272508+Gofven@users.noreply.github.com>
2025-12-11 16:11:29 +0100

    Added FieldNotBlankValidator to all model CharField and TextFields

--- flowback/common/validators.py
+++ flowback/common/validators.py
@@ -2,5 +2,5 @@ from django.core.exceptions import ValidationError
 
 
 def FieldNotBlankValidator(value):
-    if value and not value.strip():
+    if value is not None and not value.strip():
         raise ValidationError("This field cannot be blank")
